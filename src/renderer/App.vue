bf60ba5287f60dc1ed178b00924239d2ba33d517
ZhangWafer <ZhangWafer@users.noreply.github.com>
2020-09-16 20:56:38 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <firstPage id="firstPage">
+    </firstPage> -->
+    <!-- <one></one> -->
+    <router-view></router-view>
+    <!-- <cookerPage></cookerPage> -->
+  </div>
+</template>
+
+<script>
+import firstPage from '@/components/firstPage'
+import one from '@/components/one'
+import cookerPage from '@/components/cookerPage'
+
+export default {
+  name: 'wafer-project',
+  components: {
+    firstPage,
+    one,
+    cookerPage
+  }
+}
+
+</script>
+
+<style>
+#app {
+    /* font-family: 'Helvetica Neue', Helvetica, 'Hiragino Sans GB',
+        'Microsoft YaHei', Arial, sans-serif; */
+    font-family: '微软雅黑';
+}
+/* CSS */
+#firstPage {
+    height: 100%;
+}
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import router from './router'
+
+Vue.use(ElementUI)
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  components: { App },
+  template: '<App/>'
+
+}).$mount('#app')
